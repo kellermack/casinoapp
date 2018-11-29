c166be767b5f66338246184f58c7f9b3a1beb36f
jasonblo <jason@websolutionsnyc.com>
2018-11-28 17:05:24 -0700

    initial commit

---
+++ displayResults.php
@@ -0,0 +1,17 @@
+<?php
+
+    $dbServername ="localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbName = "vegaspoker";
+    
+    $conn = mysqli_connect($dbServername, $dbusername, $dbpassword, $dbName);
+
+       
+
+    
+    
+    
+   
+    
+
