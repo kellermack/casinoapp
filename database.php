01ae9bad3170b156003cd3179b48664e7bf73d04
kelle <kelle@LAPTOP-HMIIN585.home>
2018-12-03 20:28:23 -0500

    Added more to the display.php but I feel they are not correct. Or they are not in the right place.

--- database.php
+++ database.php
@@ -18,6 +18,20 @@
 
 ?>
 
+
+
+
+
+
+
+<?php
+    $dbServername ="127.0.0.1";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbName = "vegaspoker";
+    
+    $conn = mysqli_connect($dbServername, $dbusername, $dbpassword, $dbName);
+
     
     
     
