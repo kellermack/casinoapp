51516ebbb80bdd5e3c04d112a7861b87abd273d9
kelle <kelle@LAPTOP-HMIIN585.home>
2018-12-03 20:00:28 -0500

    Added more to the display.php but I feel they are not correct. Or they are not in the right place.

--- database.php
+++ database.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-
-
-
-
-
 <?php
     $dbServername ="127.0.0.1";
     $dbusername = "root";
@@ -11,6 +5,19 @@
     $dbName = "vegaspoker";
     
     $conn = mysqli_connect($dbServername, $dbusername, $dbpassword, $dbName);
+
+       
+    
+    $connection_error = mysqli_connect_error();
+    if ($connection_error != null) {
+        echo "<p>Error connecting to database: $connection_error</p>";
+        exit();
+    }
+
+
+
+?>
+
     
     
     
