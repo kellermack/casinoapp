5d220e55d45595f436488e542fbd01b1f7ae9e16
kellermack <45441341+kellermack@users.noreply.github.com>
2020-05-08 18:43:44 -0400

    Fix install issue

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 /**
  * This file will install the database table and import the data
  *
- * Run by visiting in the browser or by executing `php install.php` in the command line
+ * Run by visiting in the browser or by executing `php install.php` in the command line.
  */
 
 require('Database.class.php');
